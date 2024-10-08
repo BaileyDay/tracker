9ae420f6c90a8620c34cdbbf5be37d011752df3a
baileyday <bailey.j.day@gmail.com>
2024-10-07 22:46:01 -0400

    vercel

--- src/routes/api/players/+server.ts
+++ src/routes/api/players/+server.ts
@@ -11,26 +11,30 @@ const playerIds = [
 
 const CHROME_URL = process.env.CHROMIUM;
 
-async function getBrowser() {
-	if (process.env.VERCEL_ENV === 'production') {
-		const executablePath = await chromium?.executablePath();
+async function getPlayerData(playerId: number) {
+	let browser;
 
-		const browser = await puppeteerCore.launch({
-			args: chromium.args,
-			defaultViewport: chromium.defaultViewport,
-			executablePath,
-			headless: chromium.headless
-		});
-		return browser;
-	} else {
-		const browser = await puppeteer.launch();
-		return browser;
+	async function getBrowser() {
+		if (process.env.VERCEL_ENV === 'production') {
+			const executablePath = await chromium?.executablePath(CHROME_URL);
+
+			const browser = await puppeteerCore.launch({
+				args: chromium.args,
+				defaultViewport: chromium.defaultViewport,
+				executablePath,
+				headless: chromium.headless
+			});
+			return browser;
+		} else {
+			const browser = await puppeteer.launch();
+			return browser;
+		}
 	}
-}
 
-async function getPlayerData(playerId: number, browser: puppeteer.Browser) {
 	try {
+		browser = await getBrowser();
 		const page = await browser.newPage();
+
 		await page.goto(`https://tracklock.gg/players/${playerId}`, {
 			waitUntil: 'networkidle2',
 			timeout: 30000
@@ -54,36 +58,24 @@ async function getPlayerData(playerId: number, browser: puppeteer.Browser) {
 			return { nekoscore, avatarUrl, name, rank };
 		});
 
-		await page.close();
 		return { playerId, ...data };
 	} catch (error) {
 		console.error(`Error fetching data for player ${playerId}:`, error);
+		await browser?.close();
 		return null;
-	}
-}
-
-export const GET: RequestHandler = async () => {
-	let browser;
-	const playersData = [];
-
-	try {
-		browser = await getBrowser();
-
-		for (const playerId of playerIds) {
-			const data = await getPlayerData(playerId, browser); // Reuse the browser instance
-			if (data) {
-				playersData.push(data);
-			}
-		}
-
-		playersData.sort((a, b) => (b.nekoscore || 0) - (a.nekoscore || 0));
-	} catch (error) {
-		console.error('Error in browser operations:', error);
 	} finally {
 		if (browser) {
 			await browser.close();
 		}
 	}
+}
 
-	return json(playersData);
+export const GET: RequestHandler = async () => {
+	const promises = playerIds.map(getPlayerData);
+	const playersData = await Promise.all(promises);
+
+	const validPlayers = playersData.filter(Boolean);
+	validPlayers.sort((a, b) => (b.nekoscore || 0) - (a.nekoscore || 0));
+
+	return json(validPlayers);
 };
